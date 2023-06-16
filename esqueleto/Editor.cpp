feac5ea13ada36fec2a19ec9dcf59fc8c3e0741f
BuildTools <unconfigured@null.spigotmc.org>
2023-06-16 18:25:27 -0300

    Funciones terminadas. Casi todas comentadas. Falta revisar reemplazar_palabras Falta comentar insertar_palabras, borrar_posicion, borrar_palabra

--- esqueleto/Editor.cpp
+++ esqueleto/Editor.cpp
@@ -4,66 +4,84 @@ using std::string;
 
 
 Editor::Editor(const set<string> & conectivos) {
-    this->_editor = {};
-    this->_conectivos = conectivos;
-    this->_vocabulario = {};
-    this -> _apariciones = {};
-    this->_conteo_palabras = 0; 
+    this->_editor = {}; //O(1)
+    this->_conectivos = conectivos; //O(|conectivos|)
+    this->_vocabulario = {}; //O(1)
+    this -> _apariciones = {}; //O(1)
+    this->_conteo_palabras = 0; //O(1)
 }
+// Editor = O(1) + O(1) + O(1) O(|conectivos|) + O(1) = 4*O(1) + O(|conectivos|) = O(|conectivos|)
+
 
 bool Editor::es_conectivo(string palabra){
-    bool res = false;
-    if(this->_conectivos.find(palabra) != this->_conectivos.end()){
-        res = true;
+    bool res = false; //O(1)
+    if(this->_conectivos.find(palabra) != this->_conectivos.end()){ // O(log M) * O(1) = O(log M)
+        res = true; //O(1)
     }
-    return res;
+    return res; //O(1)
 }
+// es_conectivo = O(1) + O(log M) + O(1) = 2*O(1) + O(log M) = O(log M). Peor caso, que todas las palabras en el texto sean conectivos
 
 void Editor::actualizar_posiciones(int pos, int cantidad){
-    int pos_inicial = pos;
-    set <int> temporal = {};
-    while(pos < this->_editor.size()){
-        if(!(es_conectivo(this->_editor[pos]))){
-        auto it = this->_apariciones.find(_editor[pos]) -> second;
-
-        for(auto it2 = it.begin(); it2 != it.end(); it2++){
-            if(*it2 < pos_inicial){
-                temporal.insert(*it2);}
+        set <int> temporal = {}; //O(1)
+        // Este iterador, recorre todos los elementos de apariciones
+        for(auto it = this->_apariciones.begin(); it != this->_apariciones.end(); it++){ //O(M) * O(P) + 2 * O(1) = O(M) * O(P)
+            if(it->second == this->_apariciones[_editor[pos]]){ //O(1) * O(Max(P,P)) = O(P)
+                for(auto it2 = it->second.begin(); it2 != it->second.end();it2++){ // O(P) 
+                if(*it2 >= pos-1){//O(1)
+                    temporal.insert(*it2 + cantidad); //O(1)
+                }else{
+                    temporal.insert(*it2); //O(1)
+                }
+            }}
             else{
-            temporal.insert(*it2 + cantidad);}
+            //Este iterador, recorre todos los elementos del set<int> dentro de cada elemento de _apariciones
+            for(auto it2 = it->second.begin(); it2 != it->second.end();it2++){ //O(P)
+                if(*it2 >= pos){ //O(1)
+                    temporal.insert(*it2 + cantidad); //O(1)
+                }else{
+                    temporal.insert(*it2); //O(1)
+                }
+             }}
+            it->second = temporal; //O(1)
+            temporal = {}; //O(1)
         }
-        this->_apariciones[_editor[pos]] = temporal;}
-        pos++;}
     }
+//actualizar_posiciones = O(1) + O(M)*O(P) = O(M*P). Peor caso, que tenga que actualizar todas las posiciones de las palabras, p.ej: insertar_palabras("hola",0)
 
 string Editor::texto() const {
-    string res = "";
-    for (int i = 0; i < this->_editor.size(); i++){
-        if(i < _editor.size()-1){
-        res.append(this->_editor[i]);
-        res.append(" ");}
+    string res = ""; //O(1)
+    for (int i = 0; i < this->_editor.size(); i++){ //O(N)* (3 * O(1)) = O(N)
+        if(i < _editor.size()-1){ //O(1)
+        res.append(this->_editor[i]); //O(1)
+        res.append(" ");}//O(1)
         else{
-            res.append(this->_editor[i]);
+            res.append(this->_editor[i]);//O(1)
         }
     }
-    return res;
+    return res; //O(1)
 }
+//texto = O(1) + O(N) + O(1) = O(N). No hay peor caso, siempre va a tardar N iteraciones.
 
 const set<string>& Editor::vocabulario() const {
-    return _vocabulario;
+    return _vocabulario; //O(1)
 }
+//vocabulario = O(1)
 
 const set<string>& Editor::conectivos() const {
-    return _conectivos;
+    return _conectivos; //O(1)
 }
+//conectivos = O(1)
 
 int Editor::conteo_palabras() const { 
-	return _conteo_palabras; 
+	return _conteo_palabras; //O(1) 
 }
+//conteo_palabras = O(1)
 
 int Editor::longitud() const { 
-	return _editor.size();
+	return _editor.size(); //O(1)
 }
+//longitud = O(1)
 
 void Editor::agregar_atras(const string& oracion) {
     string temporal = "";
@@ -72,8 +90,8 @@ void Editor::agregar_atras(const string& oracion) {
              if(!(es_conectivo(temporal))){
                 this->_conteo_palabras++;
                 this->_vocabulario.insert(temporal);
-                this->_apariciones[temporal].insert(_editor.size());
             } 
+            this->_apariciones[temporal].insert(_editor.size());
             this->_editor.push_back(temporal);
             temporal = "";
         }
@@ -84,14 +102,15 @@ void Editor::agregar_atras(const string& oracion) {
     if(!(es_conectivo(temporal))){
                 this->_conteo_palabras++;
                 this->_vocabulario.insert(temporal);
-                this->_apariciones[temporal].insert(_editor.size());
             }
-            this->_editor.push_back(temporal); 
+            this->_apariciones[temporal].insert(_editor.size()); 
+            this->_editor.push_back(temporal);
 }
 
 const set<int> & Editor::buscar_palabra(const string& palabra) const {
-    return this->_apariciones.find(palabra)->second;
+    return this->_apariciones.find(palabra)->second; //O(log M)
 }
+//buscar_palabra = O(log M). Tiene este orden de complejidad, porque la funcion .find de un map tiene ese orden.
 
 void Editor::insertar_palabras(const string& oracion, int pos) {
     //Dividir la oracion e insertarlas guardando su posicion. Además modifica las variables internas
@@ -119,8 +138,8 @@ void Editor::insertar_palabras(const string& oracion, int pos) {
     if(!(es_conectivo(temporal))){
                 this->_conteo_palabras++;
                 this->_vocabulario.insert(temporal);
-                this->_apariciones[temporal].insert(pos);
             }
+            this->_apariciones[temporal].insert(pos);
             this->_editor.insert(it,temporal);
             cant_palabras++;
             pos++;
@@ -149,64 +168,67 @@ void Editor::borrar_posicion(int pos) {
     }
 
     // Actualizar las posiciones de las palabras siguientes en el mapa de apariciones
-    actualizar_posiciones(pos+1, -1);
+    actualizar_posiciones(pos, -1);
 }
 
 int Editor::borrar_palabra(const string& palabra) {
     // Pre: El string palabra no tiene espacios ni signos de puntuación.
     // Post: Se elimina la palabra indicada de todo el texto, y se devuelve la cantidad de palabras eliminadas.
-    set<int> posiciones = buscar_palabra(palabra); // Obtener las posiciones de la palabra
+    int apariciones_totales = 0;
 
-    int cantidadBorrada = 0; // Inicializar la cantidad borrada
-
-    for (int pos : posiciones) {
-        string palabraActual = _editor[pos];
-
-        if (!es_conectivo(palabraActual)) {
-            _conteo_palabras--;
-            _vocabulario.erase(palabraActual);
+    for(int i = 0; i < longitud();i++){
+        if(_editor[i] == palabra){
+            apariciones_totales++;
         }
-
-        _editor.erase(next(_editor.begin(), pos)); // Eliminar la palabra en la posición indicada
-
-        auto it = _apariciones.find(palabraActual); // Buscar la palabra en el mapa de apariciones
-        if (it != _apariciones.end()) {
-            it->second.erase(pos); // Eliminar la posición de la palabra en el mapa de apariciones
-            if (it->second.empty()) {
-                _apariciones.erase(it); // Si no quedan más apariciones de la palabra, eliminarla del mapa
-            }
-        }
-
-        // Actualizar las posiciones de las palabras siguientes en el mapa de apariciones
-        actualizar_posiciones(pos, 1);
-
-        cantidadBorrada += 1; // Incrementar la cantidad borrada
     }
+    if(apariciones_totales >= 1){
+    set<int> posiciones = buscar_palabra(palabra); // Obtener las posiciones de la palabra
+    auto it = this->_apariciones[palabra].begin();
+    for(int i = 0; i < posiciones.size(); i++){
+        borrar_posicion(*it);
+       it = this->_apariciones[palabra].begin();
+    }
+    if (!es_conectivo(palabra)) {
+            _conteo_palabras = _conteo_palabras - posiciones.size();
+            _vocabulario.erase(palabra);
+        }}
 
-    return cantidadBorrada; // Devolver la cantidad de palabras borradas
-}
+    return apariciones_totales; // Devolver la cantidad de palabras borradas
+    }
 
 void Editor::reemplazar_palabra(const string& palabra1, const string& palabra2) {
-    set<int> posiciones_p1 = buscar_palabra(palabra1);
-    auto it_p1 = posiciones_p1.begin();
+    set<int> posiciones_p1 = buscar_palabra(palabra1); //O(log M)
 
-    for(int pos : posiciones_p1){
-        this->_editor[pos] = palabra2;
-    }
-    if(es_conectivo(palabra1)){
-        if(es_conectivo(palabra1))
-        this->_vocabulario.insert(palabra2);
-        this->_apariciones[palabra2] = posiciones_p1;
-
-        this->_vocabulario.erase(palabra1);
-        this->_apariciones.erase(palabra1);
+    for(int pos : posiciones_p1){ //O(P) * O(1) = O(P)
+        this->_editor[pos] = palabra2; //O(1)
     }
 
-/* int main(){
-    Editor e({});
-    e.agregar_atras("pin uno pin dos pin tres pin cuatro pin cinco pin seis pin siete pinocho");
+    if(es_conectivo(palabra1)){ //O(log M) + O(log M) + O(P) * O(log M) = O(P) * O(log M)
+        if(es_conectivo(palabra2)){ //O(log M)
+            for(int pos : posiciones_p1){ //O(P) * (O(1) + O(Log M)) = O(P) * O(Log M))
+                this->_apariciones[palabra2].insert(pos); //O(1)
+                this->_apariciones[palabra1].erase(pos); //O(log M)
+            }
+        }
+        else{
+            for(int pos : posiciones_p1){//O(P)
+                this->_apariciones[palabra2].insert(pos); //O(1)
+                this->_apariciones[palabra1].erase(pos); //O(log M)
+            }
+            this->_conteo_palabras = this->_conteo_palabras + posiciones_p1.size(); //O(1)
+            this->_vocabulario.insert(palabra2); //O(1)
+        }}
+    
+    else{ //O(P) + O(log M) + O(log M) + O(log M) = O(P)
+        for(int pos : posiciones_p1){  //O(P)
+                this->_apariciones[palabra2].insert(pos); //O(1)
+            }
+            this->_vocabulario.erase(palabra1); //O(log M)
+            this->_apariciones.erase(palabra1); //O(log M)
+        if(es_conectivo(palabra2)){ //O(log M)
+            this->_conteo_palabras = this->_conteo_palabras - posiciones_p1.size(); //O(1)      
+        }
+        }
+    }
 
-    e.borrar_palabra("pin");
-    cout << e.texto() << endl;
-    return 0;
-}    */
+    //reemplazar_palabra = O(log M) + O(P) + O(P) * O(log M) + O(P) = O(P * log M)
