7897368d9aa26e9992d641c5d4f61b2555f9f5e3
jaimeamigo03 <102691435+jaimeamigo03@users.noreply.github.com>
2023-06-20 10:29:54 -0300

    Update Editor.cpp

--- esqueleto/Editor.cpp
+++ esqueleto/Editor.cpp
@@ -24,29 +24,29 @@ bool Editor::es_conectivo(string palabra){
 void Editor::actualizar_posiciones(int pos, int cantidad){
         set <int> temporal = {}; //O(1)
         // Este iterador, recorre todos los elementos de apariciones
-        for(auto it = this->_apariciones.begin(); it != this->_apariciones.end(); it++){ //O(M) * O(P) + 2 * O(1) = O(M) * O(P)
-            if(it->second == this->_apariciones[_editor[pos]]){ //O(1) * O(Max(P,P)) = O(P)
-                for(auto it2 = it->second.begin(); it2 != it->second.end();it2++){ // O(P) 
+        for(auto it = this->_apariciones.begin(); it != this->_apariciones.end(); it++){ //O(M) * O(P) + 2 * O(1) = O(M) * O(P log M)
+            if(it->second == this->_apariciones[_editor[pos]]){ //O(1) * O(Max([O(P) * O(log M)],[O(P) * O(log M)])) = O(P) * O(log M)
+                for(auto it2 = it->second.begin(); it2 != it->second.end();it2++){ // O(P) * O(log M)
                 if(*it2 >= pos-1){//O(1)
-                    temporal.insert(*it2 + cantidad); //O(1)
+                    temporal.insert(*it2 + cantidad); //O(log M)
                 }else{
-                    temporal.insert(*it2); //O(1)
+                    temporal.insert(*it2); //O(log M)
                 }
             }}
             else{
             //Este iterador, recorre todos los elementos del set<int> dentro de cada elemento de _apariciones
-            for(auto it2 = it->second.begin(); it2 != it->second.end();it2++){ //O(P)
+            for(auto it2 = it->second.begin(); it2 != it->second.end();it2++){ //O(P) * O(log M)
                 if(*it2 >= pos){ //O(1)
-                    temporal.insert(*it2 + cantidad); //O(1)
+                    temporal.insert(*it2 + cantidad); //O(log M)
                 }else{
-                    temporal.insert(*it2); //O(1)
+                    temporal.insert(*it2); //O(log M)
                 }
              }}
             it->second = temporal; //O(1)
             temporal = {}; //O(1)
         }
     }
-//actualizar_posiciones = O(1) + O(M)*O(P) => O(M*P). Peor caso, que tenga que actualizar todas las posiciones de las palabras, p.ej: insertar_palabras("hola",0)
+//actualizar_posiciones = O(1) + O(M)*O(P log M) => O(M*P log M). Peor caso, que tenga que actualizar todas las posiciones de las palabras, p.ej: insertar_palabras("hola",0)
 
 string Editor::texto() const {
     string res = ""; //O(1)
@@ -88,7 +88,7 @@ void Editor::agregar_atras(const string& oracion) {
         if(oracion[i] == ' '){ //O(1)
              if(!(es_conectivo(temporal))){ //O(1)
                 this->_conteo_palabras++; //O(1)
-                this->_vocabulario.insert(temporal); //O(1)
+                this->_vocabulario.insert(temporal); //O(log M)
             } 
             this->_apariciones[temporal].insert(_editor.size()); //O(log M)
             this->_editor.push_back(temporal); //O(1)
@@ -100,7 +100,7 @@ void Editor::agregar_atras(const string& oracion) {
     }
     if(!(es_conectivo(temporal))){ //O(1)
                 this->_conteo_palabras++; //O(1)
-                this->_vocabulario.insert(temporal); //O(1)
+                this->_vocabulario.insert(temporal); //O(log M)
             }
     this->_apariciones[temporal].insert(_editor.size()); //O(log M)
     this->_editor.push_back(temporal); //O(1)
@@ -117,11 +117,11 @@ void Editor::insertar_palabras(const string& oracion, int pos) {
     string temporal = ""; //O(1)
     int cant_palabras = 0; //O(1)
     auto it = this->_editor.begin() + pos; //O(1)
-    for(int i = 0; i < oracion.size(); i++){ //O(|oracion|)
-        if(oracion[i] == ' '){ //O(1)
+    for(int i = 0; i < oracion.size(); i++){ //O(|oracion|) * O(log M)
+        if(oracion[i] == ' '){ //O(1) + O(log M) => O(log M)
              if(!(es_conectivo(temporal))){ //O(1)
                 this->_conteo_palabras++; //O(1)
-                this->_vocabulario.insert(temporal); //O(1)
+                this->_vocabulario.insert(temporal); //O(log M)
                 this->_apariciones[temporal].insert(pos); //O(log M)
             } 
             this->_editor.insert(it,temporal); //O(1)
@@ -136,7 +136,7 @@ void Editor::insertar_palabras(const string& oracion, int pos) {
     }
     if(!(es_conectivo(temporal))){ //O(1)
                 this->_conteo_palabras++; //O(1)
-                this->_vocabulario.insert(temporal); //O(1)
+                this->_vocabulario.insert(temporal); //O(log M)
             }
             this->_apariciones[temporal].insert(pos); //O(log M)
             this->_editor.insert(it,temporal); //O(1)
@@ -144,9 +144,9 @@ void Editor::insertar_palabras(const string& oracion, int pos) {
             pos++; //O(1)
 
     //Modificar las posiciones de las palabras subsecuentes
-    actualizar_posiciones(pos, cant_palabras); //O(M*P)
+    actualizar_posiciones(pos, cant_palabras); //O(M*P log M)
 }
-//insertar_palabras = O(|oracion|) * O(log M) + O(log M) + O(M*P) => O(|oracion|) * O(log M) + O(M*P)
+//insertar_palabras = O(|oracion|) * O(log M) + O(log M) + O(M*P log M) => O(|oracion|) * O(log M) + O(M*P log M)
 
 void Editor::borrar_posicion(int pos) {
     // Pre: 0 ≤ posicion ≤ longitud()-1
@@ -158,19 +158,19 @@ void Editor::borrar_posicion(int pos) {
         _vocabulario.erase(palabra); //O(M)
     }  // Eliminar la palabra del conjunto de vocabulario si no es conectiva.
 
-    _editor.erase(next(_editor.begin(), pos)); //O(M)  // Eliminar la palabra en la posición indicada
+    _editor.erase(next(_editor.begin(), pos)); //O(M) Eliminar la palabra en la posición indicada
 
-    auto it = _apariciones.find(palabra); //O(log M) // Busca la palabra especificada
+    auto it = _apariciones.find(palabra); //O(log M) Busca la palabra especificada
     if (it != _apariciones.end()) { //O(M)
-        it->second.erase(pos); //O(M)  // Eliminar la posición de la palabra en el mapa de apariciones
+        it->second.erase(pos); //O(M) Eliminar la posición de la palabra en el mapa de apariciones
         if (it->second.empty()) //O(P)
-            _apariciones.erase(it); //O(1)  // Si no quedan más apariciones de la palabra, eliminarla del mapa
+            _apariciones.erase(it); //O(1) Si no quedan más apariciones de la palabra, eliminarla del mapa
     }
 
     // Actualizar las posiciones de las palabras siguientes en el mapa de apariciones
-    actualizar_posiciones(pos, -1); //O(M*P)
+    actualizar_posiciones(pos, -1); //O(M*P log M)
 }
-//borrar_posicion = O(1) + O(M) + O(M) + O(log M) + O(M) + O(P) + O(M*P) => O(M*P)
+//borrar_posicion = O(1) + O(M) + O(M) + O(log M) + O(M) + O(P) + O(M*P log M) => O(M*P log M)
 
 int Editor::borrar_palabra(const string& palabra) {
     // Pre: El string palabra no tiene espacios ni signos de puntuación.
@@ -179,7 +179,7 @@ int Editor::borrar_palabra(const string& palabra) {
     if(posiciones.size() > 0){ //O(P)
         auto it = this->_apariciones[palabra].begin(); //O(1)
         for(int i = 0; i < posiciones.size(); i++){ //O(P) * [O(M*P) + O(log M)] => O(P) * O(P*M)
-            borrar_posicion(*it); //O(M*P)
+            borrar_posicion(*it); //O(M*P log M)
             it = this->_apariciones[palabra].begin();//O(log M)
             }
         if (!es_conectivo(palabra)) { //O(1)
@@ -194,32 +194,21 @@ int Editor::borrar_palabra(const string& palabra) {
 void Editor::reemplazar_palabra(const string& palabra1, const string& palabra2) {
     set<int> posiciones_p1 = buscar_palabra(palabra1); //O(log M) .find de buscar una palabra en el mapa apariciones
 
-    for(int pos : posiciones_p1){ //O(P) * O(1) = O(P) Recorre todas las posiciones del set pos, que como maximo tiene tamaño P
+    for(int pos : posiciones_p1){ //O(P) * O(log P) => O(P log P) Recorre todas las posiciones del set pos, que como maximo tiene tamaño P
         this->_editor[pos] = palabra2; //O(1) Reemplazar una palabra en un vector tiene O(1)
-    }
-
-    if(es_conectivo(palabra1)){ //O(1) + O(P) * O(log M) => O(P) * O(log M)
-        
-        for(int pos : posiciones_p1){ //O(P) * (O(1) + O(Log M)) = O(P) * O(Log M))
-                this->_apariciones[palabra2].insert(pos); //O(1)
-                this->_apariciones[palabra1].erase(pos); //O(log M)
-            }
-        
+	this->_apariciones[palabra2].insert(pos); // O(log P)
+    }  
+    this->_apariciones.erase(palabra1); //O(log M)	
+    if(es_conectivo(palabra1)){ // O(log M) + O(log M) => O(log M)
         if(!(es_conectivo(palabra2))){ //O(1)
             this->_conteo_palabras = this->_conteo_palabras + posiciones_p1.size(); //O(1)
-            this->_vocabulario.insert(palabra2); //O(1)
+            this->_vocabulario.insert(palabra2); //O(log M)
         }
     }
-    
-    else{ //O(P) + O(log M) + O(log M) + O(1) = O(P)
-        for(int pos : posiciones_p1){  //O(P)
-                this->_apariciones[palabra2].insert(pos); //O(1)
-            }
-            this->_vocabulario.erase(palabra1); //O(log M)
-            this->_apariciones.erase(palabra1); //O(log M)
+    else{ // O(log M) + O(1) + O(1) = O(log M)
+        this->_vocabulario.erase(palabra1); //O(log M)
         if(es_conectivo(palabra2)){ //O(1)
             this->_conteo_palabras = this->_conteo_palabras - posiciones_p1.size(); //O(1)      
-        }
-        }
+        }}
     }
-    //reemplazar_palabra = O(log M) + O(P) + O(P) * O(log M) + O(P) => O(P * log M)
+    //reemplazar_palabra = O(log M) + O(P log P) + O(log M) + O(log M) => O(log M + P log P)
